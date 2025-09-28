39abfdb80b71116912cc9dc2374afad773b91c7d
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-09-28 19:54:43 +0300

    go on

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -8,19 +8,19 @@ export const options = {
      firstItemClassName: 'tui-first-child',
      lastItemClassName: 'tui-last-child',
      template: {
-         page: '<a href="#" class="box04 tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="box03 tui-page-btn tui-is-selected">{{page}}</strong>',
+         page: '<a href="#" class="buttons_other tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="button_orange tui-page-btn tui-is-selected">{{page}}</strong>',
          moveButton:
-             '<a href="#" class="box05 tui-page-btn tui-{{type}}">' +
+             '<a href="#" class="arrow tui-page-btn tui-{{type}}">' +
                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
              '</a>',
          disabledMoveButton:
-             '<span class="box06 tui-page-btn tui-is-disabled tui-{{type}}">' +
+             '<span class="arrow tui-page-btn tui-is-disabled tui-{{type}}">' +
                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
              '</span>',
          moreButton:
-             '<a href="#" class="box07 tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
+             '<a href="#" class="dots tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">. . .</span>' +
              '</a>'
      }
     };
