6161946ab1dc212646c5f9eab54fbe4af51ab876
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2026-02-23 07:56:02 +0000

    Deploying to gh-pages from @ Seeker2025/experiments11@622fafa8e57bffc18edd3076f25e3385f78572c8 🚀

--- page-07.js
+++ page-07.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("page07");const e=document.querySelector("input");console.log(e);e.addEventListener("change",o=>{console.log(o.target.checked)});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("page07  Switch");const e=document.querySelector("input");console.log(e);e.addEventListener("change",o=>{console.log(o.target.checked)});
 //# sourceMappingURL=page-07.js.map
---
+++ page-08.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("page08 Arguments");function o(){let n=null;return n=arguments[0]+arguments[1],n}console.log(o(3,2));
+//# sourceMappingURL=page-08.js.map
