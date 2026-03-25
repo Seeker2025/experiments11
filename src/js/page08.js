91e4793d3e25c06f7c1908cb17aa9688645befff
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-03-25 20:00:24 +0200

    go on

--- src/js/page08.js
+++ src/js/page08.js
@@ -15,5 +15,36 @@ function sum(){
     return a;
 };
 
-console.log(sum(3, 2));
+// console.log(sum(3, 2));
+
+
+// //оператор нулевого слияния
+// console.log(1 ?? 20);                  //1
+// console.log(0 ?? 20);                  //0
+// console.log(false ?? 20);              //false
+// console.log('' ?? 20);                 //''
+// console.log(' ' ?? 20);                //' '
+// console.log(undefined ?? 20);          //20
+// console.log(null ?? 20);               //20
+
+
+
+
+
+
+const isObj = {
+    keyOne: undefined,
+    name01: undefined,
+    girl: 'Natasha',
+}
+const{keyOne: keyTwo='Steve', name01='Bill', girl} = isObj;
+console.log(keyTwo, name01, girl); ////Steve, Bill, Natasha
+
+
+
+
+
+
+
+
 
