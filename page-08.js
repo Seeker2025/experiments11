ed6b9614bf27f5f814a1a6a157d80e017bb81d6e
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2026-03-25 18:00:43 +0000

    Deploying to gh-pages from @ Seeker2025/experiments11@91e4793d3e25c06f7c1908cb17aa9688645befff 🚀

--- page-08.js
+++ page-08.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("page08 Arguments");function o(){let n=null;return n=arguments[0]+arguments[1],n}console.log(o(3,2));
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("page08 Arguments");const e={keyOne:void 0,name01:void 0,girl:"Natasha"},{keyOne:n="Steve",name01:o="Bill",girl:i}=e;console.log(n,o,i);
 //# sourceMappingURL=page-08.js.map
