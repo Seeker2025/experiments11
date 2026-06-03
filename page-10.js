e11ad54fd940df9eed30f523dff119ea74453f43
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2026-06-03 09:13:12 +0000

    Deploying to gh-pages from @ Seeker2025/experiments11@2c04f94b4e6a610b7cb061172f775e5faeabb70f 🚀

--- page-10.js
+++ page-10.js
@@ -1,2 +1,2 @@
-import"./assets/styles-BTLSwsbK.js";console.log("page 10, Arguments, Rest");function e(...o){return o[0]+o[1]}e(3,4);e(5,6,7);const n={name:"Lena",name02:"Gena",name03:"Pete"};console.log(n);function c({name:o,name02:t,name03:a}){console.log(o,t,a)}c({...n});
+import"./assets/styles-BTLSwsbK.js";console.log("page 10, Arguments, Rest");function e(...o){return o[0]+o[1]}e(3,4);e(5,6,7);const n={name:"Lena",name02:"Gena",name03:"Pete"};console.log(n);function c({name:o,name02:t,name03:l}){console.log(o,t,l)}c({...n});console.log(3);
 //# sourceMappingURL=page-10.js.map
