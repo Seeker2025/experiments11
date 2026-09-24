c669da6be5af650b65afb320fb68d0eb43aff271
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2026-09-24 07:52:11 +0000

    Deploying to gh-pages from @ Seeker2025/experiments11@e449f490629395cd39102705946927359f909594 🚀

--- page-09.js
+++ page-09.js
@@ -1,2 +1,2 @@
-import"./assets/styles-BTLSwsbK.js";console.log("page09 splice");const o=["Petro","Vladimir","Viсtor","Leonid"],r=o.splice(2,1,"Elon");console.log(o);console.log(r);const l=["Petro","Vladimir","Elon","Leonid"],s=l.splice(2,0,"Mark");console.log(l);console.log(s);const e=["red","green","blue"];e.splice(2,0,"purple");console.log(e);
+import"./assets/styles-BTLSwsbK.js";console.log("page09 splice");const o=["Petro","Vladimir","Viсtor","Leonid"],r=o.splice(2,1,"Elon");console.log(o);console.log(r);const l=["Petro","Vladimir","Elon","Leonid"],s=l.splice(2,0,"Mark");console.log(l);console.log(s);const e=["red","green","blue"];e.splice(2,0,"purple");console.log(e);const c="яблоко, банан, апельсин",n=c.split(", ")[0];console.log(n);
 //# sourceMappingURL=page-09.js.map
